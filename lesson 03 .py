f3137c82cdb4a1a63513787ae6c24559c133adcc
aleqsandr12 <aleqsandretedoradze029@gmail.com>
2025-04-27 17:39:55 +0400

    Add files via upload

---
+++ lesson 03 .py
@@ -0,0 +1,30 @@
+#stirng  ცვლადი
+game = "aleqsandre"
+
+game =  "hello world"
+
+game = "april folls"
+
+
+#int ცვლადი
+box = 5
+
+box = 10
+
+box = 15
+
+
+#float ცვლადი
+Minecraft = 0.5
+
+Minecraft  = 0.7
+
+Minecraft  = 0.3
+
+
+
+
+
+print ("game")
+print ("box")
+print ("Minecraft ")
---
+++ lesson 03.01.py
@@ -0,0 +1,13 @@
+gameer = ("hello my name is aleqsandre and i am 10 year old bron in 2015")
+
+
+gameer = ("i have got a ps 4 i got it on my birthday")
+
+
+gammer = ("i sold my old PC becouse it was laggy")
+
+
+gemeer = ("i allways do my homework after i rest")
+
+
+print ("gammer")
